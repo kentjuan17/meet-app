9ec9efe2e851659f4419a2a2dbe13a85253907c0
kentjuan17 <kentjuan17@gmail.com>
2023-02-15 13:37:39 -0500

    modified Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import Add from '../img/addAvatar.png'
+import React from "react";
+import Add from "../img/addAvatar.png";
 
 export const Register = () => {
   return (
     <>
-    <div className="form-row">
+      <div className="form-row">
         <div className="form-container img-bg">
-            <h2>Welcome to Chat App!</h2>
-            <hr />
-            <p>Connecting you, anytime, anywhere!</p>
+          <h1>Meet App</h1>
+          <hr />
+          <p>Connecting you, anytime, anywhere!</p>
         </div>
         <div className="form-container register-form">
-            <div className="form-wrapper">
-                <span className="title">Sign Up</span>
-                <form>
-                    <input type="text" placeholder="Enter your full name" />
-                    <input type="email" placeholder="Enter your email" />
-                    <input type="password" placeholder="Enter your password" />
-                    <input style={{display: "none"}} type="file" id="avatar" />
-                    <label htmlFor="avatar">
-                        <img src={Add} alt="" />
-                        <span>Upload display picture</span>
-                    </label>
-                    <button>Sign Up</button>
-                </form>
-                <p>Already have an account? Login here.</p>
-            </div>
+          <div className="form-wrapper">
+            <span className="title">Sign Up</span>
+            <form>
+              <input type="text" placeholder="Enter your full name" />
+              <input type="email" placeholder="Enter your email" />
+              <input type="password" placeholder="Enter your password" />
+              <input style={{ display: "none" }} type="file" id="avatar" />
+              <label htmlFor="avatar">
+                <img src={Add} alt="" />
+                <span>Upload display picture</span>
+              </label>
+              <button>Sign Up</button>
+            </form>
+            <p>Already have an account? Login here.</p>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
